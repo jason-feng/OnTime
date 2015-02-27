0c7687bc56b1350502b1b41c2ed5452bfbd62fb3
Dani Gnibus <Danielle.N.Gnibus.17@dartmouth.edu>
2015-02-27 13:32:00 -0500

    added hard coded events to MainActivity

--- app/src/main/java/edu/dartmouth/cs/ontime/MainActivity.java
+++ app/src/main/java/edu/dartmouth/cs/ontime/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.graphics.drawable.BitmapDrawable;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
@@ -11,7 +12,10 @@ import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import edu.dartmouth.cs.ontime.view.SlidingTabLayout;
 
@@ -21,10 +25,7 @@ public class MainActivity extends Activity {
 
     private SlidingTabLayout slidingTabLayout;
     private ViewPager viewPager;
-    private ArrayList<Fragment> fragments;
-    private ActionTabsViewPagerAdapter myViewPagerAdapter;
-    private HomeFragment homeFrag;
-    private CreateFragment createFrag;
+    private ArrayList<Event> upcomingEvents;
     private Settings settingsFrag;
 
 
@@ -34,6 +35,49 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         LinearLayout layout =(LinearLayout)findViewById(R.id.background);
+
+        //get person based on regId of phone (from server); for now this and events are hard-coded
+
+        //sample event 1
+        Event event1 = new Event();
+        Calendar newDateTime = Calendar.getInstance();
+        newDateTime.set(2015, 3, 1, 12, 25);
+        event1.setmDateTime(newDateTime);
+        Location loc = new Location("");
+        loc.setLatitude(43.704441);
+        loc.setLongitude(-72.288693);
+        event1.setmLocation(loc);
+        upcomingEvents.add(event1);
+
+        Event event2 = new Event();
+        Calendar newDateTime2 = Calendar.getInstance();
+        newDateTime2.set(2015, 3, 2, 12, 25);
+        event2.setmDateTime(newDateTime2);
+        Location loc2 = new Location("");
+        loc2.setLatitude(43.701728);
+        loc2.setLongitude(-72.288848);
+        event2.setmLocation(loc);
+        upcomingEvents.add(event2);
+
+        Event event3 = new Event();
+        Calendar newDateTime3 = Calendar.getInstance();
+        newDateTime3.set(2015, 3, 5, 12, 25);
+        event3.setmDateTime(newDateTime3);
+        Location loc3 = new Location("");
+        loc3.setLatitude(43.704451);
+        loc3.setLongitude(-72.286643);
+        event3.setmLocation(loc3);
+        upcomingEvents.add(event3);
+
+        //TODO: iterate through upcomingEvents
+
+        //for each event in upcoming events list
+            //if it's today, add it to the today text
+            //if it's tomorrow add it to tomorrow text
+            //if it's this week, add it to this week text
+            //else add it to upcoming events overall
+
+
 
     }
 
