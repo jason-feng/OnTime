5e595e0188e3d46079968d3c7031eab887ad3867
Dani Gnibus <Danielle.N.Gnibus.17@dartmouth.edu>
2015-02-27 13:02:37 -0500

    removed all fragment stuff from main activity and started UI

--- app/src/main/java/edu/dartmouth/cs/ontime/MainActivity.java
+++ app/src/main/java/edu/dartmouth/cs/ontime/MainActivity.java
@@ -3,10 +3,13 @@ package edu.dartmouth.cs.ontime;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 
@@ -15,7 +18,7 @@ import edu.dartmouth.cs.ontime.view.SlidingTabLayout;
 
 public class MainActivity extends Activity {
 
-    //OnTime demo 1
+
     private SlidingTabLayout slidingTabLayout;
     private ViewPager viewPager;
     private ArrayList<Fragment> fragments;
@@ -30,49 +33,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       // startService(new Intent(MainActivity.this, TrackingService.class));
+        LinearLayout layout =(LinearLayout)findViewById(R.id.background);
 
-        slidingTabLayout = (SlidingTabLayout) findViewById(R.id.tab);
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-
-        if (savedInstanceState != null){
-            homeFrag = (HomeFragment) getFragmentManager().getFragment(savedInstanceState, HomeFragment.class.getName());
-            createFrag = (CreateFragment) getFragmentManager().getFragment(savedInstanceState, CreateFragment.class.getName());
-            settingsFrag = (Settings) getFragmentManager().getFragment(savedInstanceState, Settings.class.getName());
-        }
-
-        if (homeFrag == null){
-            homeFrag = new HomeFragment();
-        }
-        if (createFrag == null){
-            createFrag = new CreateFragment();
-        }
-        if (settingsFrag == null){
-            settingsFrag = new Settings();
-        }
-
-        // create a fragment list in order.
-        fragments = new ArrayList<Fragment>();
-        fragments.add(homeFrag);
-        fragments.add(createFrag);
-        fragments.add(settingsFrag);
-
-        // use FragmentPagerAdapter to bind the slidingTabLayout (tabs with different titles) and ViewPager (different pages of fragment) together.
-        myViewPagerAdapter =new ActionTabsViewPagerAdapter(getFragmentManager(),
-                fragments);
-        viewPager.setAdapter(myViewPagerAdapter);
-        viewPager.setOffscreenPageLimit(2);
-
-        // make tabs the appropriate design
-        slidingTabLayout.setDistributeEvenly(true);
-        slidingTabLayout.setViewPager(viewPager);
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        getFragmentManager().putFragment(outState, HomeFragment.class.getName(), homeFrag);
-        getFragmentManager().putFragment(outState, CreateFragment.class.getName(), createFrag);
-        getFragmentManager().putFragment(outState, Settings.class.getName(), settingsFrag);
+
     }
 }
