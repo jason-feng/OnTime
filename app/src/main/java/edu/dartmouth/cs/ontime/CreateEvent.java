705716fdbb6463d4c6ba60ff10cf9511cea6f63c
jf3ng <jason21feng@gmail.com>
2015-03-01 02:30:51 -0500

    add map fragment

--- app/src/main/java/edu/dartmouth/cs/ontime/CreateEvent.java
+++ app/src/main/java/edu/dartmouth/cs/ontime/CreateEvent.java
@@ -10,7 +10,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.text.InputType;
 import android.util.Log;
 import android.view.Menu;
@@ -21,13 +20,8 @@ import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
-
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -79,9 +73,7 @@ public class CreateEvent extends ListActivity {
                             newFragment.show(getFragmentManager(), "timePicker");
                             break;
                         case 3: //location
-//                            MapFragment fragment = new MapFragment();
-//                            FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                            startActivity(new Intent(context, MapDialogFragment.class));
+                            startActivity(new Intent(context, MapFragment.class));
                             break;
                         case 4: //Invites
                             newFragment = new NewListUsersDialogFragment()
@@ -121,68 +113,6 @@ public class CreateEvent extends ListActivity {
         }
 
         // ******************** Fragment Classes *************************//
-
-    public class MapDialogFragment extends FragmentActivity
-            implements GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
-
-        private GoogleMap mMap;
-        private TextView mTapTextView;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-//            setContentView(R.layout.events_demo);
-
-//            mTapTextView = (TextView) findViewById(R.id.tap_text);
-
-            setUpMapIfNeeded();
-        }
-
-        /**
-         * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-         * installed) and the map has not already been instantiated.. This will ensure that we only ever
-         * call {@link #setUpMap()} once when {@link #mMap} is not null.
-         * <p/>
-         * If it isn't installed {@link com.google.android.gms.maps.SupportMapFragment} (and
-         * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-         * install/update the Google Play services APK on their device.
-         * <p/>
-         * A user can return to this FragmentActivity after following the prompt and correctly
-         * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-         * have been completely destroyed during this process (it is likely that it would only be
-         * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-         * method in {@link #onResume()} to guarantee that it will be called.
-         */
-        private void setUpMapIfNeeded() {
-            // Do a null check to confirm that we have not already instantiated the map.
-            if (mMap == null) {
-                // Try to obtain the map from the SupportMapFragment.
-                mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                        .getMap();
-                // Check if we were successful in obtaining the map.
-                if (mMap != null) {
-                    setUpMap();
-                }
-            }
-        }
-
-
-        private void setUpMap() //If the setUpMapIfNeeded(); is needed then...
-        {
-            mMap.setOnMapClickListener(this);
-            mMap.setOnMapLongClickListener(this);
-        }
-
-        @Override
-        public void onMapClick(LatLng point) {
-            mTapTextView.setText("tapped, point=" + point);
-        }
-
-        @Override
-        public void onMapLongClick(LatLng point) {
-            mTapTextView.setText("long pressed, point=" + point);
-        }
-    }
 
         //provided by Android Developers for Date Picker Fragment
         public static class DatePickerFragment extends DialogFragment
--- app/src/main/java/edu/dartmouth/cs/ontime/MapFragment.java
+++ app/src/main/java/edu/dartmouth/cs/ontime/MapFragment.java
@@ -2,88 +2,73 @@ package edu.dartmouth.cs.ontime;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A fragment that launches other parts of the demo application.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends FragmentActivity
+        implements GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
 
-    MapView mMapView;
-    private GoogleMap googleMap;
+    private GoogleMap mMap;
+    private TextView mTapTextView;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // inflat and return the layout
-        View v = inflater.inflate(R.layout.fragment_map, container, false);
-        mMapView = (MapView) v.findViewById(R.id.mapView);
-        mMapView.onCreate(savedInstanceState);
-        mMapView.onResume();// needed to get the map to display immediately
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_map);
+        mTapTextView = (TextView) findViewById(R.id.tap_text);
 
-        try {
-            MapsInitializer.initialize(getActivity().getApplicationContext());
-        } catch (Exception e) {
-            e.printStackTrace();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link com.google.android.gms.maps.SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
         }
+    }
 
-        googleMap = mMapView.getMap();
-        // latitude and longitude
-        double latitude = 17.385044;
-        double longitude = 78.486671;
 
-        // create marker
-        MarkerOptions marker = new MarkerOptions().position(
-                new LatLng(latitude, longitude)).title("Hello Maps");
-
-        // Changing marker icon
-        marker.icon(BitmapDescriptorFactory
-                .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
-
-        // adding marker
-        googleMap.addMarker(marker);
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(new LatLng(17.385044, 78.486671)).zoom(12).build();
-        googleMap.animateCamera(CameraUpdateFactory
-                .newCameraPosition(cameraPosition));
-
-        // Perform any camera updates here
-        return v;
+    private void setUpMap() //If the setUpMapIfNeeded(); is needed then...
+    {
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMapLongClickListener(this);
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        mMapView.onResume();
+    public void onMapClick(LatLng point) {
+        mTapTextView.setText("tapped, point=" + point);
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        mMapView.onPause();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mMapView.onDestroy();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mMapView.onLowMemory();
+    public void onMapLongClick(LatLng point) {
+        mTapTextView.setText("long pressed, point=" + point);
     }
 }
