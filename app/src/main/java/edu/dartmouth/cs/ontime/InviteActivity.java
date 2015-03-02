bed999a606f788a5303132e22a39fcc476e5b90b
Emily Greene <Emily.C.Greene.17@dartmouth.edu>
2015-03-02 00:19:47 -0500

    Added invite activity

--- app/src/main/java/edu/dartmouth/cs/ontime/InviteActivity.java
+++ app/src/main/java/edu/dartmouth/cs/ontime/InviteActivity.java
@@ -2,15 +2,23 @@ package edu.dartmouth.cs.ontime;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class InviteActivity extends Activity {
 
+    public ListView mList;
+    static final String[] invites = new String[] {"Bowling \n Nick invited you: accepted"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite);
+
+        mList = (ListView)findViewById(R.id.listTd);
+
+        mList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, invites));
+
+        MainActivity.ListUtils.setDynamicHeight(mList);
     }
 }
