d6ad76255e87727d20c0b8ab254dd151aa1d8e46
Emily Greene <Emily.C.Greene.17@dartmouth.edu>
2015-03-02 08:36:55 -0500

    Fixed invite page bug

--- app/src/main/java/edu/dartmouth/cs/ontime/InviteActivity.java
+++ app/src/main/java/edu/dartmouth/cs/ontime/InviteActivity.java
@@ -9,13 +9,13 @@ import android.widget.ListView;
 public class InviteActivity extends Activity {
 
     public ListView mList;
-    static final String[] invites = new String[] {"Bowling \n Nick invited you: accepted"};
+    static final String[] invites = new String[] {"Bowling \nNick invited you: accepted"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite);
 
-        mList = (ListView)findViewById(R.id.listTd);
+        mList = (ListView)findViewById(R.id.list);
 
         mList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, invites));
 
