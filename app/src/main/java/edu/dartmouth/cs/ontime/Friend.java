92b87d9c9637d21739fb4aa2fd33a29547cda290
Emily Greene <Emily.C.Greene.17@dartmouth.edu>
2015-03-02 20:51:00 -0500

    Profile pics and friends lists accessible

---
+++ app/src/main/java/edu/dartmouth/cs/ontime/Friend.java
@@ -0,0 +1,20 @@
+package edu.dartmouth.cs.ontime;
+
+import java.net.URL;
+
+/**
+ * Created by garygreene on 3/2/15.
+ */
+public class Friend {
+    String name;
+    String id;
+    URL url;
+
+    public Friend(String name, String id,URL url) {
+        this.name = name;
+        this.id = id;
+        this.url = url;
+    }
+
+
+}
--- app/src/main/java/edu/dartmouth/cs/ontime/SettingsFragment.java
+++ app/src/main/java/edu/dartmouth/cs/ontime/SettingsFragment.java
@@ -2,18 +2,35 @@ package edu.dartmouth.cs.ontime;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphObject;
 import com.facebook.widget.LoginButton;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -24,7 +41,10 @@ public class SettingsFragment extends Fragment {
     private static final String TAG = "SettingsFragment";
     private UiLifecycleHelper uiHelper;
     private Context mContext;
-    public ArrayList<FacebookFriend> friends;
+    public ArrayList<Friend> friendsArray;
+    Friend me = null;
+    TextView name;
+    ImageView propic;
 
 
     private Session.StatusCallback callback = new Session.StatusCallback() {
@@ -36,9 +56,11 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        friendsArray = new ArrayList<Friend>();
 
         uiHelper = new UiLifecycleHelper(getActivity(), callback);
         uiHelper.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -51,6 +73,8 @@ public class SettingsFragment extends Fragment {
         authButton.setReadPermissions(Arrays.asList("public_profile"));
         authButton.setReadPermissions(Arrays.asList("user_friends"));
         authButton.setFragment(this);
+        name = (TextView) view.findViewById(R.id.name);
+        propic = (ImageView) view.findViewById(R.id.propic);
 
         return view;
     }
@@ -58,52 +82,76 @@ public class SettingsFragment extends Fragment {
     private void onSessionStateChange(Session session, SessionState state, Exception exception) {
         if (state.isOpened()) {
             Log.i(TAG, "Logged in...");
-//            new Request(
-//                    session,
-//                    "/me",
-//                    null,
-//                    HttpMethod.GET,
-//                    new Request.Callback() {
-//                        public void onCompleted(Response response) {
-//                            GraphObject graphObject = response.getGraphObject();
-//                            JSONArray dataArray = (JSONArray) graphObject.getProperty("data");
-//                            if (dataArray.length() > 0) {
-//
-//                                // Ensure the user has at least one friend ...
-//                                for (int i = 0; i < dataArray.length(); i++) {
-//
-//                                    JSONObject jsonObject = dataArray.optJSONObject(i);
-//                                    FacebookFriend me = new FacebookFriend(jsonObject);
-//                                    Log.d("me",me.name);
-//                                }
-//                            }
-//                        }
-//                    }
-//            ).executeAsync();
-//            new Request(
-//                    session,
-//                    "/me/friends",
-//                    null,
-//                    HttpMethod.GET,
-//                    new Request.Callback() {
-//                        public void onCompleted(Response response) {
-//                                /* handle the result */
-//                            GraphObject graphObject = response.getGraphObject();
-//                            JSONArray dataArray = (JSONArray) graphObject.getProperty("data");
-//                            if (dataArray.length() > 0) {
-//
-//                                // Ensure the user has at least one friend ...
-//                                for (int i = 0; i < dataArray.length(); i++) {
-//
-//                                    JSONObject jsonObject = dataArray.optJSONObject(i);
-//                                    FacebookFriend facebookFriend = new FacebookFriend(jsonObject);
-//                                    friends.add(facebookFriend);
-//                                }
-//                            }
-//
-//                        }
-//                    }
-//            ).executeAsync();
+            new Request(
+                    session,
+                    "/me",
+                    null,
+                    HttpMethod.GET,
+                    new Request.Callback() {
+                        public void onCompleted(Response response) {
+                            GraphObject graphObject = response.getGraphObject();
+                            JSONObject object = graphObject.getInnerJSONObject();
+                            try {
+                                URL imgUrl = new URL("https://graph.facebook.com/"
+                                        + object.getString("id") + "/picture?type=large");
+                                Log.d("url",imgUrl.toString());
+                                me = new Friend(object.getString("name"),object.getString("id"),imgUrl);
+                                new DownloadImageTask(propic)
+                                        .execute(imgUrl.toString());
+
+                                name.setText("Logged in as: " +object.getString("name"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            } catch (MalformedURLException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            Log.d("me",me.name);
+
+                        }
+                    }
+            ).executeAsync();
+
+            new Request(
+                    session,
+                    "/me/friends",
+                    null,
+                    HttpMethod.GET,
+                    new Request.Callback() {
+                        public void onCompleted(Response response) {
+                                /* handle the result */
+                            GraphObject graphObject = response.getGraphObject();
+                            JSONObject json = graphObject.getInnerJSONObject();
+                            JSONArray friends = null;
+                            try {
+                                friends = json.getJSONArray("data");
+                                Log.d("hello",friends.toString());
+                                if (friends.length() > 0) {
+
+                                    // Ensure the user has at least one friend ...
+                                    for (int i = 0; i < friends.length(); i++) {
+                                        JSONObject friend = friends.getJSONObject(i);
+                                        String name = friend.getString("name");
+                                        String id = friend.getString("id");
+                                        Log.d("name",name);
+                                        URL imgUrl = new URL("http://graph.facebook.com/"
+                                                + friend.getString("id") + "/picture?type=large");
+                                        friendsArray.add(new Friend(friend.getString("name"),friend.getString("id"),imgUrl));
+
+                                    }
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            } catch (MalformedURLException e) {
+                                e.printStackTrace();
+                            }
+
+
+                        }
+                    }
+            ).executeAsync();
 
         } else if (state.isClosed()) {
             Log.i(TAG, "Logged out...");
@@ -152,5 +200,30 @@ public class SettingsFragment extends Fragment {
         uiHelper.onSaveInstanceState(outState);
     }
 
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
 }
 
