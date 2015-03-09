8f423614adf1ecd80cb1db60efdacc006bf5739f
jf3ng <jason21feng@gmail.com>
2015-03-09 00:26:24 -0400

    fixed bug with null fields

--- app/src/main/java/edu/dartmouth/cs/ontime/EventDisplayActivity.java
+++ app/src/main/java/edu/dartmouth/cs/ontime/EventDisplayActivity.java
@@ -81,7 +81,7 @@ public class EventDisplayActivity extends FragmentActivity implements OnMapReady
     public void onLocationChanged(Location location) {
         Log.d(TAG, "onLocationChanged");
         current_location = new ParseGeoPoint(location.getLatitude(), location.getLongitude());
-        if (distance == -1.0) {
+        if (userDistances.get(position) == -1.0) {
             distance = current_location.distanceInMilesTo(finalGeoPoint);
             init_distances.set(position,distance);
             userLocations.set(position, current_location);
